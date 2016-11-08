840d1e6b5b69852e92f4c53cbd67bbb451b1f2d4
Сергей Садовский <serge@sadowsky.spb.ru>
2016-11-09 01:54:18 +0300

    Assignment 4 changing loop statement to for/in

--- assignments/assignment4/assignment4-solution-starter/harder/script.js
+++ assignments/assignment4/assignment4-solution-starter/harder/script.js
@@ -42,7 +42,7 @@ WARNING!!! WARNING!!!
   // using the 'speak' method or either helloSpeaker's or byeSpeaker's
   // 'speak' method.
   // See Lecture 50, part 1
-  for (var i=0; i < names.length; i++) {
+  for (var i in names) {
 
     // STEP 11:
     // Retrieve the first letter of the current name in the loop.
